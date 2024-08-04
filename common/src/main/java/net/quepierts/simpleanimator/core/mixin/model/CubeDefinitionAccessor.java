226bcb2903b406db140de55d9c8648157bcc9742
Louis_Quepierts <LouisQuepierts@outlook.com>
2024-08-04 23:54:45 +0800

    Fix some codes

--- common/src/main/java/net/quepierts/simpleanimator/core/mixin/model/CubeDefinitionAccessor.java
+++ common/src/main/java/net/quepierts/simpleanimator/core/mixin/model/CubeDefinitionAccessor.java
@@ -5,7 +5,6 @@ import net.minecraft.client.model.geom.builders.CubeDeformation;
 import net.minecraft.core.Direction;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 import java.util.Set;
