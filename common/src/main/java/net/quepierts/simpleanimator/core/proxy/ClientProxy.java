b5788c30ba80bbfc94bb1de6205960873a2db978
Louis_Quepierts <LouisQuepierts@outlook.com>
2024-08-25 15:17:54 +0800

    1.2.5 fix code

--- common/src/main/java/net/quepierts/simpleanimator/core/proxy/ClientProxy.java
+++ common/src/main/java/net/quepierts/simpleanimator/core/proxy/ClientProxy.java
@@ -1,15 +1,8 @@
 package net.quepierts.simpleanimator.core.proxy;
 
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.client.player.RemotePlayer;
-import net.minecraft.resources.ResourceLocation;
-import net.quepierts.simpleanimator.api.IAnimateHandler;
-import net.quepierts.simpleanimator.core.animation.Animator;
-import net.quepierts.simpleanimator.core.client.ClientAnimator;
 import net.quepierts.simpleanimator.core.client.ClientAnimatorManager;
 import net.quepierts.simpleanimator.core.client.ClientPlayerNavigator;
 import net.quepierts.simpleanimator.core.config.ClientConfiguration;
-import org.joml.Vector3f;
 
 public class ClientProxy extends CommonProxy {
     private final ClientPlayerNavigator navigator;
@@ -39,13 +32,5 @@ public class ClientProxy extends CommonProxy {
 
     public ClientConfiguration getClientConfiguration() {
         return config;
-    }
-
-    public void example(AbstractClientPlayer player) {
-        Animator animator = ((IAnimateHandler) player).simpleanimator$getAnimator();
-        animator.play(ResourceLocation.fromNamespaceAndPath("id", "anim_name"));
-
-        ClientAnimator clientAnimator = (ClientAnimator) animator;
-        Vector3f name = clientAnimator.getVariable("name").getAsVector3f();
     }
 }
